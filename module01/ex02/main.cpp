54b1f7f8a4c78f0e08f1938409b02ceb45779eae
olivia <oliviawalsh073@gmail.com>
2022-12-13 13:47:43 +0100

    Add module01/ex02

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -6,7 +6,26 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 20:30:34 by owalsh            #+#    #+#             */
-/*   Updated: 2022/12/10 20:30:35 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/12/13 13:47:16 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+	// std::string& stringREF; // IMPOSSIBLE
+	
+	std::cout << "Adresse de la string en memoire: " << &str << std::endl;	
+	std::cout << "Adresse stockée dans stringPTR: " << stringPTR << std::endl;	
+	std::cout << "Adresse stockée dans stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Valeur de la string: " << str << std::endl;
+	std::cout << "Valeur pointée par stringPTR: " << *stringPTR << std::endl;	
+	std::cout << "Valeur pointée par stringREF: " << stringREF << std::endl;
+
+	return (0);
+}
