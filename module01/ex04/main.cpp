7b4afccb53ac6ebe643f7a82fee5afbb4b3dee0e
olivia <oliviawalsh073@gmail.com>
2022-12-14 15:17:26 +0100

    Add error cases handling

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,13 +6,14 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 20:30:56 by owalsh            #+#    #+#             */
-/*   Updated: 2022/12/14 13:26:02 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/12/14 15:14:58 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <cstdlib>
 
 void	checkLine(std::string &line, std::string s1, std::string s2)
 {
@@ -37,13 +38,18 @@ void	read_file(std::string filename, std::string s1, std::string s2)
 	char outfileName[filename.length() + 1];
 	strcpy(outfileName, filename.c_str());
 	
-	std::ofstream outfile(outfileName);
 	std::ifstream infile;
 	infile.open(infileName, std::ios::out);
 	if (!infile)
 	{
-		std::cout << "File could not be open" << std::endl;
-		exit(1);
+		std::cout << "Error: infile: " << strerror(errno) << std::endl;
+		std::exit(1);
+	}
+	std::ofstream outfile(outfileName);
+	if (!outfile)
+	{
+		std::cout << "Error: outfile: " << strerror(errno) << std::endl;
+		std::exit(1);
 	}
 	std::string line;
 	while (getline(infile, line))
@@ -53,6 +59,45 @@ void	read_file(std::string filename, std::string s1, std::string s2)
 	}
 	outfile.close();
 	infile.close();
+}
+
+bool	validFilename(std::string filename)
+{
+	bool	onlySpaces = true;
+	
+	for (int i = 0; filename[i]; i++)
+	{
+		if (!isprint(filename[i]))
+			return (false);
+		if (!isspace(filename[i]))
+			onlySpaces = false;
+	}
+	if (!onlySpaces)
+		return (true);
+	return (false);
+}
+
+void	checkArgs(char **argv, std::string &filename, std::string &s1, std::string &s2)
+{
+	filename = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];
+	if (filename.empty())
+	{
+		std::cout << "filename cannot be empty" << std::endl;
+		std::exit(1);
+	}
+	if (!validFilename(filename))
+	{
+		std::cout << "filename is not a valid filename" << std::endl;
+		std::exit(1);
+	}
+	if (s1.empty())
+	{
+		std::cout << "s1 cannot be empty" << std::endl;
+		std::exit(1);
+	}
+	
 }
 
 int	main(int argc, char **argv)
@@ -66,9 +111,7 @@ int	main(int argc, char **argv)
 		std::cout << "Program requires 3 arguments" << std::endl;
 		return (1);
 	}
-	filename = argv[1];
-	s1 = argv[2];
-	s2 = argv[3];
+	checkArgs(argv, filename, s1, s2);
 	read_file(filename, s1, s2);
 	return (0);
 }
