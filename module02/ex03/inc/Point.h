ff374a66498e41ce94928ca3b588471979de8092
owalsh <owalsh@student.42.fr>
2022-12-17 21:19:37 +0100

    Add latest changes to exBSP

--- module02/ex03/inc/Point.h
+++ module02/ex03/inc/Point.h
@@ -6,7 +6,7 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 16:08:15 by owalsh            #+#    #+#             */
-/*   Updated: 2022/12/16 17:41:31 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/12/17 21:11:12 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 class Point
 {
 	private:
-		const Fixed x;	
-		const Fixed y;
+		Fixed const x;	
+		Fixed const y;
 
 	public:
 		Point() : x(0), y(0) {};
@@ -25,5 +25,9 @@ class Point
 		Point ( const Point & rhs);
 		~Point();
 
+		Point & operator=( const Point & rhs );
+
+		const Fixed getX();
+		const Fixed getY();
 
 };
--- module02/ex03/src/Point.cpp
+++ module02/ex03/src/Point.cpp
@@ -6,9 +6,43 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 16:08:10 by owalsh            #+#    #+#             */
-/*   Updated: 2022/12/16 17:41:27 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/12/17 21:13:57 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.h"
 
+Point::Point() : x(0), y(0) 
+{
+	
+}
+
+Point::Point( const float x1, const float y1) : x(x1), y(y1)
+{
+
+}
+
+Point::Point ( const Point & rhs)
+{
+	*this = rhs;
+}
+
+Point & Point::operator=( const Point & rhs )
+{
+	return *this;
+}
+
+Point::~Point()
+{
+	
+}
+
+const Fixed Point::getX()
+{
+	return x;
+}
+
+const Fixed Point::getY()
+{
+	return y;
+}
