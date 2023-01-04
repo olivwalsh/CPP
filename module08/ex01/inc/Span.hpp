07e8a260e835ce506ca08d02903008680887a299
olivwalsh <oliviawalsh073@gmail.com>
2023-01-04 12:26:15 +0100

    Add module08/ex01

--- module08/ex01/inc/Span.hpp
+++ module08/ex01/inc/Span.hpp
@@ -6,7 +6,42 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 21:01:25 by owalsh            #+#    #+#             */
-/*   Updated: 2023/01/03 21:01:26 by owalsh           ###   ########.fr       */
+/*   Updated: 2023/01/04 12:16:30 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <iostream>
+# include <list>
+
+class Span
+{
+	public:
+		Span(unsigned int max_sixe);
+		Span(const Span &);
+		~Span(void);
+		
+		Span & operator=(const Span &);
+
+		void	addNumber(int n);
+		int		shortestSpan();
+		int		longestSpan();
+
+	private:
+		unsigned int	_max_size;
+		std::list<int>	_list;
+
+	public:
+		class SpanIsFull : public std::exception
+		{
+			virtual const char * what() const throw();
+		};
+		class CannotFindSpan : public std::exception
+		{
+			virtual const char * what() const throw();	
+		};
+};
+
+#endif
--- module08/ex01/src/Span.cpp
+++ module08/ex01/src/Span.cpp
@@ -6,7 +6,72 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 21:01:28 by owalsh            #+#    #+#             */
-/*   Updated: 2023/01/03 21:01:29 by owalsh           ###   ########.fr       */
+/*   Updated: 2023/01/04 12:23:30 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+Span::Span(unsigned int max_sixe)
+	: _max_size(max_sixe)
+{
+}
+
+Span::Span(const Span &ref)
+{
+	*this = ref;
+}
+
+Span::~Span(void)
+{
+}
+
+Span & Span::operator=(const Span &ref)
+{
+	_max_size = ref._max_size;
+	return *this;
+}
+
+void Span::addNumber(int n)
+{
+	if (_list.size() + 1 > _max_size)
+	{
+		throw SpanIsFull();
+		return;
+	}
+	_list.push_back(n);
+}
+
+int Span::shortestSpan()
+{
+	if (_list.size() <= 1)
+	{
+		throw CannotFindSpan();
+		return ;
+	}
+	_list.sort();
+	std::list<int>::iterator it = _list.begin();
+	std::advance(it, 1);
+	return (*it - *_list.begin());
+}
+
+int Span::longestSpan()
+{
+	if (_list.size() <= 1)
+	{
+		throw CannotFindSpan();
+		return ;
+	}
+	_list.sort();
+	return (*_list.end() - *_list.begin());
+}
+
+const char * Span::SpanIsFull::what() const throw()
+{
+	return "Cannot add more numbers, Span already contains max size!";
+}
+ 
+const char * Span::CannotFindSpan::what() const throw()
+{
+	return "Not enough numbers in class, to calculate this span!";
+}
